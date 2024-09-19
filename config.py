3aefc51e1f51138d9276297ad39fbf43d4044265
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-09-19 16:40:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", "1234"))
+    APP_ID = int(os.environ.get("APP_ID", "16457832"))
 
     API_HASH = os.environ.get("API_HASH", "")
 
