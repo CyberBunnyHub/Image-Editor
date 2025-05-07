70da10423ca99c2b513da0d97853264cfe36b657
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-07 19:24:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
+from flask import Flask
 import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 # By @TroJanzHEX
 from pyrogram import Client
