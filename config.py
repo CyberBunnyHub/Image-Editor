4e40448c9d146a7fd5a8d4f7ccf74ea2e5ce30a4
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-07 18:47:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ class Config:
     API_ID = "14853951"
     API_HASH = "0a33bc287078d4dace12aaecc8e73545"
     BOT_TOKEN = "7850386310:AAFHTXecLRW9AdVwBLGKlqe6aZlBZckMFvs"
-    REMOVE_BG = "vzxJSig9pi5j4xvDX3ykqzyc"
+    RemoveBG_API = "vzxJSig9pi5j4xvDX3ykqzyc"
