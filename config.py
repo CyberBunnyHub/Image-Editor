c3cb33964361c581c3e0e88b0c29482dacaea2af
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-07 19:12:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    API_ID = "14853951"
+    APP_ID = "14853951"
     API_HASH = "0a33bc287078d4dace12aaecc8e73545"
     TG_BOT_TOKEN = "7850386310:AAFHTXecLRW9AdVwBLGKlqe6aZlBZckMFvs"
     RemoveBG_API = "vzxJSig9pi5j4xvDX3ykqzyc"
