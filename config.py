e1744b0aa3e00a9ea4e9e99c71d9c24ee1c1093d
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-07 18:46:32 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    API_ID = "14853951"
+    API_HASH = "0a33bc287078d4dace12aaecc8e73545"
+    BOT_TOKEN = "7850386310:AAFHTXecLRW9AdVwBLGKlqe6aZlBZckMFvs"
+    REMOVE_BG = "vzxJSig9pi5j4xvDX3ykqzyc"
