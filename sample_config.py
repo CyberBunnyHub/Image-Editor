c25122012feecc3b32b313e540c589c88c0e3e68
Bunny <Cy3er3unny.Official@gmail.com>
2025-05-08 12:16:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,11 +5,11 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7850386310:AAEeCeBvPdkwThjHSOjHecEhWfzI4DQ8JsM")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", "14853951"))
 
     API_HASH = os.environ.get("API_HASH", "")
 
     # Get this api from https://www.remove.bg/b/background-removal-api
-    RemoveBG_API = os.environ.get("RemoveBG_API", "")
+    RemoveBG_API = os.environ.get("RemoveBG_API", "vzxJSig9pi5j4xvDX3ykqzyc")
